a8c8ea9b7ef0e8f5dc025f2fcba55d58c3ea4719
Dmitriy Dekhanov <dmitriydekhanov@gmail.com>
2025-04-03 13:13:57 +0400

    feat: Export HTTP error classes from `@tinkoff/request-plugin-protocol-http`

--- packages/plugin-protocol-http/src/index.ts
+++ packages/plugin-protocol-http/src/index.ts
@@ -1,5 +1,6 @@
 export * from './utils';
 export * from './validators';
+export * from './errors'
 export { HttpMethods } from './constants';
 export { default } from './http';
 export type { Query, QuerySerializer } from '@tinkoff/request-url-utils';
